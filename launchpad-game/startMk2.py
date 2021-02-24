53b1428d4947d214175820fd6b7f4abc21145c9e
Jannls <69260883+Jannls@users.noreply.github.com>
2021-02-24 16:11:53 +0100

    Update startMk2.py

--- launchpad-game/startMk2.py
+++ launchpad-game/startMk2.py
@@ -1,7 +1,4 @@
-from threading import Thread
-import LaunchpadPro
-import LaunchpadMk2
-import launchpad_py as launchpad
+from LaunchpadMk2 import LauchpadMk2
 import playsound
 import atexit
 import time
@@ -9,38 +6,17 @@ import json
 import os
 
 
-def check_launchpad():
-    global lp
-    mode = None
-
-    if LaunchpadPro.LaunchpadPro().Check(0):
-        lp = LaunchpadPro.LaunchpadPro()
-        if lp.Open(0):
-            mode = "Pro"
-    elif LaunchpadMk2.LaunchpadMk2().Check(0):
-        lp = LaunchpadMk2.LaunchpadMk2()
-        if lp.Open(0):
-            mode = "Mk2"
-    return lp
-
-
 def on_exit():
-    os.system(f"python {os.getcwd()}/start.py")
-
-
-def play_sound(fname: str):
-    playsound.playsound(fname)
+    os.system(f"python \"{os.path.dirname(__file__)}\"")
 
 
 class Game:
     def __init__(self):
-        self.lp = check_launchpad()
-        if self.lp.Check(0):
-            self.lp.Reset()
+        self.lp = LaunchpadMk2()
+        self.lp.Reset()
         self.lp.register_on_button_press(on_button=self.on_button_press)
         self.press = []
         self.cur_level = 0
-        self.can_gameover = False
         self.isDead = False
         self.levels = json.loads(open("levels.json", "r").read())
         self.next_level()
@@ -51,41 +27,38 @@ class Game:
                               self.levels[self.cur_level]["color"][1], self.levels[self.cur_level]["color"][2])
         time.sleep(1)
         self.lp.Reset()
-        self.can_gameover = True
         for i in self.levels[self.cur_level]["pos"]:
-            self.lp.LedCtrlXY(i[0] - 1, i[1], self.levels[self.cur_level]["color"][0],
+            self.lp.LedCtrlXY(i[0], i[1], self.levels[self.cur_level]["color"][0],
                               self.levels[self.cur_level]["color"][1], self.levels[self.cur_level]["color"][2])
-            time.sleep(0.015)
+            time.sleep(0.05)
         while not self.levels[self.cur_level]["pos"] == self.press:
             if self.isDead:
                 self.on_death()
         self.cur_level += 1
         self.press = []
-        Thread(target=play_sound, args=("sounds/win.wav",)).start()
+        playsound.playsound("win.wav")
         if self.cur_level + 1 > len(self.levels):
-            self.can_gameover = False
             self.on_win()
         else:
-            self.can_gameover = False
             self.next_level()
 
     def on_button_press(self, x, y, pres):
         if pres > 0:
-            if self.levels[self.cur_level]["pos"][len(self.press)] != [x, y] and self.can_gameover:
+            if self.levels[self.cur_level]["pos"][len(self.press)] != [x, y]:
                 self.isDead = True
                 return
-            if [x, y] in self.press and self.can_gameover:
+            if [x, y] in self.press:
                 self.isDead = True
                 return
-            if self.can_gameover:
-                self.lp.LedCtrlXY(x - 1, y, 0, 255, 0)
-                self.press.append([x, y])
-                if self.levels[self.cur_level]["pos"][-1] != [x, y]:
-                    Thread(target=play_sound, args=("sounds/correct.wav",)).start()
+            self.lp.LedCtrlXY(x, y, 0, 255, 0)
+            self.press.append([x, y])
+            if self.levels[self.cur_level]["pos"][-1] != [x, y]:
+                playsound.playsound("correct.wav")
 
     def on_win(self):
         self.lp.Reset()
         self.lp.LedCtrlString("Win", 0, 255, 0, direction=self.lp.SCROLL_LEFT, waitms=50)
+        self.lp.continue_listener = False
         exit()
 
     def on_death(self):
