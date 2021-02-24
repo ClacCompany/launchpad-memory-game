26f356a2eb053475b8993794a576756fdc425fd5
Shanox <67274738+Shanox17@users.noreply.github.com>
2021-02-24 11:01:57 +0100

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+title Setup - Launchpad Memory Game
+color e
+pip install numpy
+pip install launchpad_py
+pip install opencv-python
+pip install playsound
+pip install keyboard
+echo Finished!
